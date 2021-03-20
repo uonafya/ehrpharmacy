3977d2f0e557ea6efca9db9a4095252bdc0c1ecd
rosaga <richardjohnzranger@gmail.com>
2021-03-20 12:44:21 +0300

    edit code to pull payment category based on Uuid

--- omod/src/main/java/org/openmrs/module/pharmacyapp/page/controller/PrintDrugOrderPageController.java
+++ omod/src/main/java/org/openmrs/module/pharmacyapp/page/controller/PrintDrugOrderPageController.java
@@ -219,17 +219,17 @@ public class PrintDrugOrderPageController {
 
             for (PersonAttribute pa : pas) {
                 PersonAttributeType attributeType = pa.getAttributeType();
-                PersonAttributeType personAttributePCT = hcs.getPersonAttributeTypeByName("Paying Category Type");
-                PersonAttributeType personAttributeNPCT = hcs.getPersonAttributeTypeByName("Non-Paying Category Type");
-                PersonAttributeType personAttributeSSCT = hcs.getPersonAttributeTypeByName("Special Scheme Category Type");
-                if (attributeType.getPersonAttributeTypeId() == personAttributePCT.getPersonAttributeTypeId()) {
+                if (hcs.getPersonAttributeTypeByName("Paying Category Type") == Context.getPersonService()
+                        .getPersonAttributeTypeByUuid("e191b0b8-f069-11ea-b498-2bfd800847e8")) {
                     model.addAttribute("paymentCategory", "PAYING");
                     model.addAttribute("paymentSubCategory", pa.getValue());
-                } else if (attributeType.getPersonAttributeTypeId() == personAttributeNPCT.getPersonAttributeTypeId()) {
-                    model.addAttribute("paymentCategory", "NON-PAYING");
+                } else if (hcs.getPersonAttributeTypeByName("Non-Paying Category Type") == Context.getPersonService()
+                        .getPersonAttributeTypeByUuid("0a8ae818-f06a-11ea-ab82-2f183f30d954")) {
+                    model.addAttribute("paymentCategoryName", "NON-PAYING");
                     model.addAttribute("paymentSubCategory", pa.getValue());
-                } else if (attributeType.getPersonAttributeTypeId() == personAttributeSSCT.getPersonAttributeTypeId()) {
-                    model.addAttribute("paymentCategory", "SPECIAL SCHEMES");
+                } else if (hcs.getPersonAttributeTypeByName("Special Scheme Category Type") == Context.getPersonService()
+                        .getPersonAttributeTypeByUuid("341ee8fa-f06a-11ea-aca0-03d040bd88c8")) {
+                    model.addAttribute("paymentCategoryName", "SPECIAL SCHEMES");
                     model.addAttribute("paymentSubCategory", pa.getValue());
                 }
             }
